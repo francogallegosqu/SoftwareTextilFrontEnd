78ac367166fec9b0320b9b3d3111c02de27fde44
francogallegos02@gmail.com <francogallegos02@gmail.com>
2022-06-17 00:21:50 -0500

    changing landing page

--- src/modules/web/Web3.vue
+++ src/modules/web/Web3.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <!-- NavBar -->
-    <b-navbar
+     <!-- NavBar -->
+    <b-row class="header-navbar d-flex justify-content-center align-item-center">
+      <b-col cols="10">
+        <b-navbar
       :toggleable="currentSizeDirect"
       type="light"
       variant="light"
@@ -18,57 +20,55 @@
 
       <b-navbar-toggle target="navbar-toggle-collapse">
         <template #default="{ expanded }">
-          <feather-icon v-if="expanded" icon="ChevronsUpIcon" size="25" />
-          <feather-icon v-else icon="ChevronsDownIcon" size="25" />
+          <feather-icon class="text-success" v-if="expanded" icon="ChevronsUpIcon" size="25" />
+          <feather-icon class="text-success" v-else icon="ChevronsDownIcon" size="25" />
         </template>
       </b-navbar-toggle>
 
       <b-collapse id="navbar-toggle-collapse" is-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item href="#"
-            ><b-button class="web-button" @click="redirectToSignIn"
+        <b-navbar-nav class="ml-auto " :class="currentSizeDirect?'d-flex flex-column':''">
+          <b-nav-item href="#">
+            <b-link class="web-button-1" @click="redirectToSignIn"
               >Iniciar Sesión
-            </b-button></b-nav-item
+            </b-link></b-nav-item
           >
           <b-nav-item
-            ><b-button class="web-button" @click="redirectToSignUp">
+            ><b-link class="web-button-1" @click="redirectToSignUp">
               Regístrate gratis
-            </b-button></b-nav-item
+            </b-link></b-nav-item
           >
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
-    <!-- Home Background Image -->
-    <div>
-      <!-- Home -->
+      </b-col>
+    </b-row>
+
+    <!-- Home -->
       <b-row class="d-flex justify-content-center align-item-center mt-1 pt-2">
         <b-col cols="10" md="5" lg="5" xl="5" class="d-flex align-item-center">
-          <b-card no-body>
-            <b-card-body
-              class="d-flex flex-column justify-content-between align-items-center"
-            >
-              <div class="w-100">
-                <h1 style="font-size: 3.5rem; font-weight: 700">
-                  Encuentra a tu próximo socio de negocios
-                </h1>
-                <p style="font-size: 1.2rem">
-                  Somos una web que busca ayudarte a encontrar proveedores y
-                  clientes manera segura y gratuita haciendo crecer tu negocio.
-                </p>
-              </div>
-              <div class="w-100">
-                <b-button class="web-button" @click="redirectToSignUp"
-                  >Registrate ahora <feather-icon icon="ArrowUpRightIcon"
-                /></b-button>
-              </div>
-            </b-card-body>
-          </b-card>
+          <div
+            class="d-flex flex-column justify-content-center align-items-center"
+          >
+            <div class="w-100">
+              <h1 style="font-size: 3.5rem; font-weight: 700">
+                Encuentra a tu próximo socio de negocios
+              </h1>
+              <p style="font-size: 1.2rem">
+                Somos una web que busca ayudarte a encontrar proveedores y
+                clientes manera segura y gratuita haciendo crecer tu negocio.
+              </p>
+            </div>
+            <div class="w-100">
+              <b-button class="web-button" @click="redirectToSignUp"
+                >Registrate ahora <feather-icon icon="ArrowUpRightIcon"
+              /></b-button>
+            </div>
+          </div>
         </b-col>
         <b-col cols="10" md="5" lg="5" xl="5">
           <b-img src="@/assets/images/manos.png" alt="Manos" fluid />
         </b-col>
       </b-row>
-    </div>
     <!-- About -->
     <b-row class="d-flex justify-content-center align-item-center mt-1">
       <b-col cols="10" sm="6" md="5" lg="5" xl="5">
@@ -114,7 +114,7 @@
         </div>
       </b-col>
     </b-row>
-    <!-- Steps -->
+    <!-- Steps Client -->
     <b-row class="mt-2 d-flex justify-content-center">
       <b-col cols="10">
         <b-card no-body class="card-content-cards">
@@ -126,7 +126,7 @@
           </h2>
           <h3 class="text-white text-center">Si buscas proveedores</h3>
 
-          <b-row class="m-1 ">
+          <b-row class="m-1 d-flex justify-content-center">
             <b-col cols="10" sm="5" md="4" lg="3" xl="3" class="mb-1">
               <b-card no-body class="sub-card-body">
                 <div class="sub-card-number">01</div>
@@ -142,20 +142,72 @@
               <b-card no-body class="sub-card-body">
                 <div class="sub-card-number">02</div>
                 <div :class="cardContentItem">
-                  <h3 >Crea tu pedido</h3>
-                  <p>
-                    Registra el producto o servicio que estas buscando.
-                  </p>
+                  <h3>Crea tu pedido</h3>
+                  <p>Registra el producto o servicio que estas buscando.</p>
                 </div>
               </b-card>
             </b-col>
             <b-col cols="10" sm="5" md="4" lg="3" xl="3" class="mb-1">
               <b-card no-body class="sub-card-body">
                 <div class="sub-card-number">03</div>
-                  <div :class="cardContentItem">
+                <div :class="cardContentItem">
                   <h3>Recibe alertas</h3>
-                  <p >
-                    Te notificaremos al encontrar los proveedores que mejor se adecuen a lo que necesitas.
+                  <p>
+                    Te notificaremos al encontrar los proveedores que mejor se
+                    adecuen a lo que necesitas.
+                  </p>
+                </div>
+              </b-card>
+            </b-col>
+          </b-row>
+        </b-card>
+      </b-col>
+    </b-row>
+    <!-- Steps Provider -->
+    <b-row class="mt-2 d-flex justify-content-center">
+      <b-col cols="10 ">
+        <b-card no-body class="card-content-cards">
+          <h3 class="text-white text-center mt-2">Si buscas clientes</h3>
+
+          <b-row class="m-1 d-flex justify-content-center">
+            <b-col cols="10" sm="5" md="4" lg="3" xl="3" class="mb-1">
+              <b-card no-body class="sub-card-body">
+                <div class="sub-card-number">01</div>
+                <div :class="cardContentItem">
+                  <h3>Registra tu negocio</h3>
+                  <p>
+                    Crea un perfil para que otros negocios puedan conocerte.
+                  </p>
+                </div>
+              </b-card>
+            </b-col>
+            <b-col cols="10" sm="5" md="4" lg="3" xl="3" class="mb-1">
+              <b-card no-body class="sub-card-body">
+                <div class="sub-card-number">02</div>
+                <div :class="cardContentItem">
+                  <h3>Publica tus productos</h3>
+                  <p>Registra lo que ofreces a tus clientes.</p>
+                </div>
+              </b-card>
+            </b-col>
+            <b-col cols="10" sm="5" md="4" lg="3" xl="3" class="mb-1">
+              <b-card no-body class="sub-card-body">
+                <div class="sub-card-number">03</div>
+                <div :class="cardContentItem">
+                  <h3>Recibe alertas</h3>
+                  <p>
+                    Te notificaremos cuando un cliente este interesado en ti.
+                  </p>
+                </div>
+              </b-card>
+            </b-col>
+            <b-col cols="10" sm="5" md="4" lg="3" xl="3" class="mb-1">
+              <b-card no-body class="sub-card-body">
+                <div class="sub-card-number">04</div>
+                <div :class="cardContentItem">
+                  <h3>Responde los pedidos</h3>
+                  <p>
+                    Para que los clientes sepan que puedes atenderlos y se pongan en contacto contigo.
                   </p>
                 </div>
               </b-card>
@@ -209,7 +261,7 @@ a.navbar-brand {
   min-width: 50%;
   max-width: 500px;
 }
-.navbar {
+.header-navbar {
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2) !important;
 }
 
@@ -229,6 +281,15 @@ a.navbar-brand {
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+}
+
+.web-button-1 {
+  color: hsl(152, 24%, 32%, 1);
+  font-weight: 700;
+  font-size: 1rem;
+}
+.web-button-1:hover {
+  color: #006414;
 }
 
 .web-button {
@@ -255,7 +316,7 @@ a.navbar-brand {
   flex: auto;
   transition: 0.3s;
 }
-.sub-card-body{
-  height:100%
+.sub-card-body {
+  height: 100%;
 }
 </style>
