bd91f2a65a4b0268df59f0a1d9a588136b8fb6e5
francogallegos <francogallegos02@outlook.com>
2022-08-03 14:06:58 -0500

    adding dist

---
+++ dist/js/chunk-2d222993.eadb73c4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d222993"],{cefa:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("Module")])])}],l=t("2877"),r={},a=Object(l["a"])(r,c,u,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d222993.eadb73c4.js.map
