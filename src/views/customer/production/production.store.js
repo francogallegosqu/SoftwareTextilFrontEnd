82d2c9147a5a60fffc71b924f416d333ce6c604f
Oscar Quiroz <65783932+oscarqochoa@users.noreply.github.com>
2022-08-06 14:39:18 -0500

    Production edit

--- src/views/customer/production/production.store.js
+++ src/views/customer/production/production.store.js
@@ -3,6 +3,7 @@ import productionService from "@/services/production/production.service"
 import productionFabricService from "@/services/production/production-fabric.service"
 import productionAccessoryService from "@/services/production/production-accessory.service"
 import productionServiceService from "@/services/production/production-service.service"
+import productionDatasheetService from "@/services/production/production-datasheet.service"
 
 const state = {
 
@@ -180,6 +181,14 @@ const actions = {
     async A_DELETE_PRODUCTION_SERVICE({ commit }, id) {
         try {
             const response = await productionServiceService.deleteService(id)
+            return response
+        } catch (error) {
+            throw error
+        }
+    },
+    async A_REGISTER_PRODUCTION_DATASHEET({ commit }, body) {
+        try {
+            const response = await productionDatasheetService.registerDatasheet(body)
             return response
         } catch (error) {
             throw error
